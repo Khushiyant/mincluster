cee86f9909a98d0883475612c5bb817446d78e3b
Khushiyant <khushiyant2002@gmail.com>
2023-10-26 16:01:30 +0530

    chore(add): build,dist--gitignore

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.0.0'
+VERSION = '0.0.1'
 DESCRIPTION = 'Cluster minimization package'
 LONG_DESCRIPTION = 'A package that allows to minimize the maximum intercluster distance'
 
